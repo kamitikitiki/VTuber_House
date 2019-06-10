6e436772f6ea4740b9d584da48969ff7b3c928b3
kamitikitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-06-10 22:15:47 +0900

    push test

---
+++ Assets/Resource/Scripts/Hand_Trigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand_Trigger : MonoBehaviour
+{
+    public SteamVR_Input_Sources HandType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
+        {
+            Debug.Log("ok");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "HaveItem")
+        {
+        }
+    }
+}
