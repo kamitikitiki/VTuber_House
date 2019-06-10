cfeffb2563911b736346ab48a513e3d753d8c333
kamitikitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-06-11 01:37:49 +0900

    オブジェクトをつかめる！
    コントローラーでオブジェクトをつかんで投げられるようにした！
    前のよりみんながいじりやすい形でまとめてぷっしゅ！

--- Assets/Resource/Scripts/Hand_Trigger.cs
+++ Assets/Resource/Scripts/Hand_Trigger.cs
@@ -6,26 +6,42 @@ using Valve.VR;
 public class Hand_Trigger : MonoBehaviour
 {
     public SteamVR_Input_Sources HandType;
+    private FixedJoint Joint = null;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Joint = gameObject.GetComponent<FixedJoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
+        if (Joint.connectedBody != null)
         {
-            Debug.Log("ok");
+            SteamVR_TrackedObject tc = gameObject.GetComponent<SteamVR_TrackedObject>();
+            
+            if (SteamVR_Actions.default_GrabPinch.GetStateUp(HandType))
+            {
+                Rigidbody hand = GetComponent<Rigidbody>();
+                Rigidbody releaseItem = Joint.connectedBody;
+                Joint.connectedBody = null;
+                releaseItem.velocity = SteamVR_Actions.default_Pose.GetVelocity(HandType);
+                releaseItem.angularVelocity = SteamVR_Actions.default_Pose.GetAngularVelocity(HandType);
+                //releaseItem.maxAngularVelocity = 
+            }
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "HaveItem")
+        if (other.gameObject.tag == "HaveItem")
         {
+            if (SteamVR_Actions.default_GrabPinch.GetStateDown(HandType))
+            {
+                Debug.Log("grab");
+                Joint.connectedBody = other.gameObject.GetComponent<Rigidbody>();
+            }
         }
     }
 }
