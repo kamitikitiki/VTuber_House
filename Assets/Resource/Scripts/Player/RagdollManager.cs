b6532f20c84680d72f54cc38624e5f1ce188750f
kamitikitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-06-17 02:57:30 +0900

    カメラプレハブ調整、モデルとラグドールテスト

---
+++ Assets/Resource/Scripts/Player/RagdollManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class RagdollManager : MonoBehaviour
+{
+    public GameObject root = null;
+    public GameObject root_rag = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.LeftHand))
+        {
+            if(root.active == true)
+            {
+                root.active = false;
+                root_rag.active = true;
+            }
+            else
+            {
+                root.active = true;
+                root_rag.active = false;
+            }
+        }
+    }
+}
