8fd4d5645fa4f8fb72ae43163e0ad8563a794f52
kamitikitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-06-26 01:41:12 +0900

    test

---
+++ Assets/Resource/Scripts/Manager/PhotonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonManager : Photon.MonoBehaviour
+{
+    private bool KeyLock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KeyLock = false;
+        Debug.Log("start");
+        PhotonConnect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void  PhotonConnect()
+    {
+        PhotonNetwork.ConnectUsingSettings("v1.0");
+        Debug.Log("connect");
+    }
+
+
+}
