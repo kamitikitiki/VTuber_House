fcad1d293574990868d92ea985eeb33b63021147
kamitikitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-07-04 02:00:57 +0900

    player vr manager
    スクリプト変更

--- Assets/Resource/Scripts/Player/PlayerVRManager.cs
+++ Assets/Resource/Scripts/Player/PlayerVRManager.cs
@@ -12,7 +12,8 @@ public class PlayerVRManager : MonoBehaviourPunCallbacks
 
     public override void OnEnable()
     {
-        if(GetComponent<PhotonView>().IsMine)
+        GameObject playerModel = null;
+        if (GetComponent<PhotonView>().IsMine)
         {
             //左手取得とスクリプトOn
             transform.GetChild(0).GetComponent<SteamVR_Behaviour_Pose>().enabled = true;
@@ -20,25 +21,28 @@ public class PlayerVRManager : MonoBehaviourPunCallbacks
             transform.GetChild(1).GetComponent<SteamVR_Behaviour_Pose>().enabled = true;
             //頭の取得とスクリプトOn
             transform.GetChild(2).GetComponent<Camera>().enabled = true;
+
+            //モデルオブジェクト作成
+            Vector3 pos = Vector3.zero;
+            Quaternion qua = new Quaternion(0, 0, 0, 1);
+            playerModel = PhotonNetwork.Instantiate("PunPrefabs/" + ModelName, pos, qua);
         }
 
-        //モデルオブジェクト作成
-        Vector3 pos = Vector3.zero;
-        Quaternion qua = new Quaternion(0, 0, 0, 1);
-        GameObject playerModel = PhotonNetwork.Instantiate("PunPrefabs/" + ModelName, pos, qua);
+        if(playerModel != null)
+        {
+            //モデルに設定するオブジェクト取得
+            GameObject hand_L = transform.GetChild(0).gameObject;
+            GameObject hand_R = transform.GetChild(1).gameObject;
+            GameObject camera = transform.GetChild(2).gameObject;
 
-        //モデルに設定するオブジェクト取得
-        GameObject hand_L = transform.GetChild(0).gameObject;
-        GameObject hand_R = transform.GetChild(1).gameObject;
-        GameObject camera = transform.GetChild(2).gameObject;
+            //ラグドールにカメラ設定
+            playerModel.GetComponent<RagdollManager>().Camera = camera;
 
-        //ラグドールにカメラ設定
-        playerModel.GetComponent<RagdollManager>().Camera = camera;
-
-        //IK設定
-        VRIK ik = playerModel.GetComponent<VRIK>();
-        ik.solver.spine.headTarget = camera.transform.GetChild(0);
-        ik.solver.leftArm.target = hand_L.transform.GetChild(0);
-        ik.solver.rightArm.target = hand_R.transform.GetChild(0);
+            //IK設定
+            VRIK ik = playerModel.GetComponent<VRIK>();
+            ik.solver.spine.headTarget = camera.transform.GetChild(0);
+            ik.solver.leftArm.target = hand_L.transform.GetChild(0);
+            ik.solver.rightArm.target = hand_R.transform.GetChild(0);
+        }
     }
 }
