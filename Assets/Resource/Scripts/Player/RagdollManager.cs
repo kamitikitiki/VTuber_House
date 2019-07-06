75fce078e1da75c82c81f55ae596c9007f252253
kamitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-07-06 23:04:54 +0900

    プレイヤー同期処理追加
    ラグドールと操作の切り替え同期の実装テスト

--- Assets/Resource/Scripts/Player/RagdollManager.cs
+++ Assets/Resource/Scripts/Player/RagdollManager.cs
@@ -3,57 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 using RootMotion.FinalIK;
+using Photon.Pun; 
 
 public class RagdollManager : MonoBehaviour
 {
-    public GameObject Camera;
+    public GameObject PlayerRig;
     public GameObject Head;
     public GameObject[] RagdollBone;
 
     Vector3 basePos = Vector3.zero;
     Quaternion baseRota;
-
-    public void OnRagdoll()
-    {
-        Head.GetComponent<Rigidbody>().useGravity = true;
-        Head.GetComponent<Rigidbody>().isKinematic = false;
-        Head.GetComponent<Collider>().enabled = true;
-        for(int i = 0; i < RagdollBone.Length; i++)
-        {
-            RagdollBone[i].GetComponent<Rigidbody>().useGravity = true;
-            RagdollBone[i].GetComponent<Rigidbody>().isKinematic = false;
-            RagdollBone[i].GetComponent<Collider>().enabled = true;
-        }
-
-        GetComponent<VRIK>().enabled = false;
-
-    }
-
-    public void OffRagdoll()
-    {
-        Head.GetComponent<Rigidbody>().useGravity = false;
-        Head.GetComponent<Rigidbody>().isKinematic = true;
-        Head.GetComponent<Collider>().enabled = false;
-        for (int i = 0; i < RagdollBone.Length; i++)
-        {
-            RagdollBone[i].GetComponent<Rigidbody>().useGravity = false;
-            RagdollBone[i].GetComponent<Rigidbody>().isKinematic = true;
-            RagdollBone[i].GetComponent<Collider>().enabled = false;
-        }
-
-        GetComponent<VRIK>().enabled = true;
-        Camera.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
-    }
+    Photon.Pun.PhotonView m_PhotonView;
 
     // Start is called before the first frame update
     void Start()
     {
-        basePos = Camera.transform.position;
+        basePos = PlayerRig.transform.position;
     }
 
     private void OnEnable()
     {
-        basePos = Camera.transform.position;
+        //basePos = Camera.transform.position;
+        m_PhotonView = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
@@ -64,10 +35,12 @@ public class RagdollManager : MonoBehaviour
             if(Head.GetComponent<Rigidbody>().isKinematic == true)
             {
                 OnRagdoll();
+                m_PhotonView.RPC("OnRagdoll", RpcTarget.AllViaServer);
             }
             else
             {
                 OffRagdoll();
+                m_PhotonView.RPC("OffRagdoll", RpcTarget.AllViaServer);
             }
         }
     }
@@ -85,20 +58,49 @@ public class RagdollManager : MonoBehaviour
                 trackingPos.z * scale.z
             );
 
-            Vector3 headr;
-            headr.x = Head.transform.rotation.x;
-            headr.y = Head.transform.rotation.y;
-            headr.z = Head.transform.rotation.z;
-
-            Camera.transform.rotation = Head.transform.rotation;
+            PlayerRig.transform.rotation = Head.transform.rotation;
 
             // 回転
-            //trackingPos = Camera.transform.rotation * trackingPos;
-            trackingPos = Head.transform.rotation * trackingPos;
+            trackingPos = PlayerRig.transform.rotation * trackingPos;
 
             // 固定したい位置から hmd の位置を
             // 差し引いて実質 hmd の移動を無効化する
-            Camera.transform.position = Head.transform.position + basePos - trackingPos;
+            PlayerRig.transform.position = Head.transform.position + basePos - trackingPos;
         }
+    }
+
+    //新規実装関数
+    [PunRPC]
+    public void OnRagdoll()
+    {
+        Head.GetComponent<Rigidbody>().useGravity = true;
+        Head.GetComponent<Rigidbody>().isKinematic = false;
+        Head.GetComponent<Collider>().enabled = true;
+        for (int i = 0; i < RagdollBone.Length; i++)
+        {
+            RagdollBone[i].GetComponent<Rigidbody>().useGravity = true;
+            RagdollBone[i].GetComponent<Rigidbody>().isKinematic = false;
+            RagdollBone[i].GetComponent<Collider>().enabled = true;
+        }
+
+        GetComponent<VRIK>().enabled = false;
+
+    }
+
+    [PunRPC]
+    public void OffRagdoll()
+    {
+        Head.GetComponent<Rigidbody>().useGravity = false;
+        Head.GetComponent<Rigidbody>().isKinematic = true;
+        Head.GetComponent<Collider>().enabled = false;
+        for (int i = 0; i < RagdollBone.Length; i++)
+        {
+            RagdollBone[i].GetComponent<Rigidbody>().useGravity = false;
+            RagdollBone[i].GetComponent<Rigidbody>().isKinematic = true;
+            RagdollBone[i].GetComponent<Collider>().enabled = false;
+        }
+
+        GetComponent<VRIK>().enabled = true;
+        PlayerRig.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
     }
 }
