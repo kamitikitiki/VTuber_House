cf9fec1f020496d10b2de60e594a3afc4df23594
kamitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-07-14 17:11:59 +0900

    ラグドール修正２

--- Assets/Resource/Scripts/Player/RagdollManager.cs
+++ Assets/Resource/Scripts/Player/RagdollManager.cs
@@ -61,10 +61,10 @@ public class RagdollManager : MonoBehaviour
                 }
             }
 
-            //if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
-            //{
-            //    SetRagdoll(true, 0);
-            //}
+            if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
+            {
+                SetRagdoll(true, 0);
+            }
         }
     }
 
