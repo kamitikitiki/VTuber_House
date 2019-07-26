9df677906723224069f1bd7a72ea4025aef87c41
kamitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-07-27 08:33:52 +0900

    tubo add

--- Assets/Prefabs/PlayObject/Tubo/TuboObject.cs
+++ Assets/Prefabs/PlayObject/Tubo/TuboObject.cs
@@ -8,8 +8,6 @@ public class TuboObject : MonoBehaviour
     public Transform m_Body;
     public Transform m_Saki;
 
-    public float MoveDis;
-
     //ハンマー移動座標
     public Transform m_TargetPos;
 
@@ -17,7 +15,9 @@ public class TuboObject : MonoBehaviour
     private ConfigurableJoint m_TuboJoint;
     private ConfigurableJoint m_BodyJoint;
 
+    //ハンマーの動く遊び
     //前回のtargetPosition
+    public float MoveDis;
     private Vector3 m_BeforeTargetPos;
 
     // Start is called before the first frame update
@@ -32,22 +32,31 @@ public class TuboObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //２点のどっちが近いか計算
-        //ボディとハンマーの距離
-        float saki_len = Vector3.Distance(m_Saki.position, m_Body.position);
-        float target_len = Vector3.Distance(m_TargetPos.position, m_Body.position);
+        //前回動かした座標との距離
+        float move_len = Vector3.Distance(m_BeforeTargetPos, m_TargetPos.position);
+        //ハンマーとターゲットの距離
+        float interval_len = Vector3.Distance(m_Saki.position, m_TargetPos.position);
 
-        m_BodyJoint.targetPosition = m_TargetPos.position - m_Saki.position;
+        if (move_len >= MoveDis && interval_len >= 0.2)
+        {
+            //２点のどっちが近いか計算
+            //ボディとハンマーの距離
+            float saki_len = Vector3.Distance(m_Saki.position, m_Body.position);
+            float target_len = Vector3.Distance(m_TargetPos.position, m_Body.position);
 
-        float velo = 0;
-        if (m_TargetPos.position.y - m_Saki.position.y > 0)
-            velo = 1.0f;
-        else
-            velo = -1.0f;
+            m_BodyJoint.targetPosition = m_TargetPos.position - m_Saki.position;
 
+            float velo = 0;
+            if (m_TargetPos.position.y - m_Saki.position.y > 0)
+                velo = 1.0f;
+            else
+                velo = -1.0f;
 
-        Vector3 v_Velo = Vector3.zero;
-        v_Velo.y = velo;
-        m_BodyJoint.targetVelocity = v_Velo;
+            Vector3 v_Velo = Vector3.zero;
+            v_Velo.y = velo;
+            m_BodyJoint.targetVelocity = v_Velo;
+        }
+
+        m_BeforeTargetPos = m_TargetPos.position;
     }
 }
