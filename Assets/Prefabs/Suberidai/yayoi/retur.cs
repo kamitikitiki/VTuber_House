db814ae1b45d4abd812233eb83c14454cb5be59f
yayoi0413 <52097747+yayoi0413@users.noreply.github.com>
2019-08-12 02:15:36 +0900

    コース追加

---
+++ Assets/Prefabs/Suberidai/yayoi/retur.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class retur : MonoBehaviour
+{
+    void Start(){
+      
+    }
+
+    void Update()
+    {
+     
+    }
+    //Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+         transform.Rotate (new Vector3(0, 0, 1));
+    }
+}
