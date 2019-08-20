00616193b3b85b552aaacae9eae5f7ac47f3f1ec
kamitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-08-21 01:12:00 +0900

    滑り台で使用するカメラの切り替え処理作成

---
+++ Assets/Resource/Scripts/Gimmick/Suberidai/SledFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SledFollow : MonoBehaviour
+{
+    public Transform sled;
+
+    private bool f_SledFollow = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(f_SledFollow == true)
+        {
+            transform.LookAt(sled);
+        }
+    }
+
+    public void changeFollowFlag(bool follow)
+    {
+        f_SledFollow = follow;
+    }
+}
---
+++ Assets/Resource/Scripts/Gimmick/Suberidai/SuberidaiCameraChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuberidaiCameraChange : MonoBehaviour
+{
+    public Transform camera;
+    public Transform camera_pos;
+
+    public bool CameraLook;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Sled")
+        {
+            camera.SetPositionAndRotation(camera_pos.position, camera_pos.rotation);
+            camera.GetComponent<SledFollow>().changeFollowFlag(CameraLook);
+        }
+    }
+}
