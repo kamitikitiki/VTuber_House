199d7cd9295314d1b6eb30c7141baa6f85ff1ecc
mogsk666 <51482728+mogsk666@users.noreply.github.com>
2019-08-24 06:06:21 +0900

    やく５００

--- Assets/Resource/Scripts/Gimmick/Suberidai/mogskGimmick/Gate.cs
+++ Assets/Resource/Scripts/Gimmick/Suberidai/mogskGimmick/Gate.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> Open_obs;
+    public List<GameObject> Close_obs;
+
+    public GameObject LeftGate;
+    public GameObject RightGate;
+
+    public float speed;
+    public float movemax;
+
+    private bool Gate_flag;
+    private float move;
+
+    private void Start()
     {
-        
+        move = 0;
+        Gate_flag = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if(Gate_flag)
+        {
+            if(move <= movemax)
+            {
+                Vector3 lgp = LeftGate.transform.position;
+                lgp.x += speed;
+                LeftGate.transform.position = lgp;
+
+                Vector3 rgp = RightGate.transform.position;
+                rgp.x -= speed;
+                RightGate.transform.position = rgp;
+
+                move += speed;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject open_ob in Open_obs)
+        {
+            open_ob.SetActive(true);
+        }
+        foreach (GameObject close_ob in Close_obs)
+        {
+            close_ob.SetActive(false);
+        }
+        Gate_flag = true;
+
     }
 }
