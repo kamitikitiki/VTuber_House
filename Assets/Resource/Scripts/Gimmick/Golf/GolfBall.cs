05f26ac4fabdc1ac5519e494038c04264a1b782b
kamitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-08-25 22:22:45 +0900

    滑り台プレハブ編集

---
+++ Assets/Resource/Scripts/Gimmick/Golf/GolfBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 velo = Vector3.zero;
+            velo.z = 300;
+            BallmoveStart(velo);
+        }
+    }
+
+    //ボールを打つ
+    public void BallmoveStart(Vector3 velo)
+    {
+        transform.GetComponent<Rigidbody>().AddForce(velo);
+    }
+}
