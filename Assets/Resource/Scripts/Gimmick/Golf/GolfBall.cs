5bc17c16c5766d180b95a11cb97dd8ef1638bcb5
kamitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-08-26 02:27:56 +0900

    ゴルフ用スクリプト作成
    ボールの移動と、プレーヤーの移動周り
    コースの引っかかり修正

--- Assets/Resource/Scripts/Gimmick/Golf/GolfBall.cs
+++ Assets/Resource/Scripts/Gimmick/Golf/GolfBall.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class GolfBall : MonoBehaviour
 {
+
+    public Transform PlayerTransform;
+
+    private bool m_Shoot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Shoot = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+
+        if (m_Shoot == true)
+        {
+            //ボールの動作停止検知
+            if (transform.GetComponent<Rigidbody>().velocity == Vector3.zero)
+            {
+                m_Shoot = false;
+                Debug.Log("Ball Stop");
+            }
+        }
+
+        //デバッグ用処理
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Vector3 velo = Vector3.zero;
             velo.z = 300;
@@ -21,9 +38,16 @@ public class GolfBall : MonoBehaviour
         }
     }
 
+    //プレイヤーをボールの座標に合わせる
+    public void SetPlayerPos()
+    {
+        PlayerTransform.position = this.transform.position;
+    }
+
     //ボールを打つ
     public void BallmoveStart(Vector3 velo)
     {
+        m_Shoot = true;
         transform.GetComponent<Rigidbody>().AddForce(velo);
     }
 }
