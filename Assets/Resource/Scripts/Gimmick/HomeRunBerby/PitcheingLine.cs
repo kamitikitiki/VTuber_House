5b5dbb76fc98e2f20ace4a9ad2fe0b002d140ce3
mogsk666 <51482728+mogsk666@users.noreply.github.com>
2019-09-05 23:01:37 +0900

    ピッチャー編集

---
+++ Assets/Resource/Scripts/Gimmick/HomeRunBerby/PitcheingLine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RouteParameter
+{
+    public Transform MovePoint;
+    public float time;
+}
+
+/*
+public interface IAutoMovement
+{
+    void setMovementVelocity(Vector3 velocity);
+
+    void onMovementStart();
+    void onMovementEnd();
+}
+*/
+
+public class PitcheingLine : MonoBehaviour
+{
+    [SerializeField]
+    protected List<RouteParameter> routes;
+}
---
+++ Assets/Resource/Scripts/Gimmick/HomeRunBerby/RoutePointEditer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoutePointEditer : MonoBehaviour
+{
+    public bool draw_sphere;
+    public float sphere_radius;
+    public Color sphereColor = Color.blue;
+
+    private void OnDrawGizmos()
+    {
+        if(draw_sphere)
+        {
+            Gizmos.color = sphereColor;
+            Gizmos.DrawSphere(transform.position, sphere_radius);
+        }
+    }
+}
