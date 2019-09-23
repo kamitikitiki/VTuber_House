f2820e142107b8e08ec7b6068e4bb6a66162ae4c
kamitiki <51053090+kamitikitiki@users.noreply.github.com>
2019-09-23 15:46:12 +0900

    add

--- Assets/Resource/Scripts/Player/PlayerCollider.cs
+++ Assets/Resource/Scripts/Player/PlayerCollider.cs
@@ -9,12 +9,12 @@ public class PlayerCollider : MonoBehaviour
 
     private Transform m_RootObject;
     private RagdollManager m_Ragdoll;
-    private PhotonView m_View;
+    //private PhotonView m_View;
 
     private void MyStateInit()
     {
         m_RootObject = transform.root.transform;
-        m_View = m_RootObject.GetComponent<PhotonView>();
+        //m_View = m_RootObject.GetComponent<PhotonView>();
         m_Ragdoll = m_RootObject.GetChild(1).GetComponent<RagdollManager>();
         m_IsInitialize = true;
     }
@@ -33,7 +33,7 @@ public class PlayerCollider : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        if(m_View.IsMine)
+        //if(m_View.IsMine)
         {
             if (other.tag == "Death")
             {
